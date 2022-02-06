3662b52319693874d97cc558a0a51f71d8e385d9
Meir Garfinkel <ygarfink2@student.touro.edu>
2022-02-06 17:04:51 -0500

    Added Javadoc for the command pattern

--- badbm/src/main/java/edu/touro/mco152/bm/ICommand.java
+++ badbm/src/main/java/edu/touro/mco152/bm/ICommand.java
@@ -1,5 +1,8 @@
 package edu.touro.mco152.bm;
 
+/**
+ * interface for commands
+ */
 public interface ICommand {
     void execute();
 
--- badbm/src/test/java/edu/touro/mco152/bm/CommandTest.java
+++ badbm/src/test/java/edu/touro/mco152/bm/CommandTest.java
@@ -14,6 +14,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+/**
+ * unit tests for command pattern
+ */
 public class CommandTest implements IuI{
 
     private int percentComplete = 0;
